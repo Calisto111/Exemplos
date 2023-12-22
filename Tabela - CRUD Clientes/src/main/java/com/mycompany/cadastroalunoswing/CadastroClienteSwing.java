617b7942aa2931d27c0a2c6ba8d01e3551db48c2
Calisto111 <matheusfcalisto@hotmail.com>
2023-12-22 13:22:55 -0300

    commit tabela clientes criacao

---
+++ Tabela - CRUD Clientes/src/main/java/com/mycompany/cadastroalunoswing/CadastroClienteSwing.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cadastroalunoswing;
+
+/**
+ *
+ * @author Matheus
+ */
+public class CadastroClienteSwing {
+
+    public static void main(String[] args) {
+        TelaPrincipal tela = new TelaPrincipal();
+        tela.setVisible(true);
+    }
+}
