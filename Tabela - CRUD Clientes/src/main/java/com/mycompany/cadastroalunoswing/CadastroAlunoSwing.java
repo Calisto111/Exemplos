f87ea144f047c47a965e977b9623eeb22950f491
Calisto111 <matheusfcalisto@hotmail.com>
2023-12-22 12:42:54 -0300

    tabela clientes utilizando swing

---
+++ Tabela - CRUD Clientes/src/main/java/com/mycompany/cadastroalunoswing/CadastroAlunoSwing.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cadastroalunoswing;
+
+/**
+ *
+ * @author Matheus
+ */
+public class CadastroAlunoSwing {
+
+    public static void main(String[] args) {
+        TelaPrincipal tela = new TelaPrincipal();
+        tela.setVisible(true);
+    }
+}
